59f300d467b14b25156d086ac17e0f73687fc027
Karolina Leczycka <karolina.leczycka93@gmail.com>
2020-04-02 15:23:15 +0200

    sprawdzenie liczby2

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var x=30;
+var y=10;
+
+function liczba(x,y){
+    return x,y;
+}
+if ((x==20)||(y==20)){
+    console.log("true");
+} else if ((x+y)<=20){
+    console.log("true");
+} else {
+    console.log();
+}
